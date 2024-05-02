e66158dad7ba71ac9b334cb2868ce68488344664
João Dias <joaoeduardodias123@gmail.com>
2024-05-02 10:41:01 -0300

    feat: initial create page home

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,5 @@
-import { Heading } from '@ignite-ui/react'
 import { Roboto } from 'next/font/google'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const roboto = Roboto({ weight: ['400', '500', '700'], subsets: ['latin'] })
-export default function Home() {
-  return (
-    <>
-      <Heading as="h1">Hello world</Heading>
-    </>
-  )
-}
+export { default } from './home'
